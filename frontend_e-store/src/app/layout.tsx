7066c80895c4f71869c45e9ff934a144c978df2a
semnic <thenriqueleite277@gmail.com>
2025-03-20 22:10:43 -0300

    feat:  add header

--- frontend_e-store/src/app/layout.tsx
+++ frontend_e-store/src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import "./globals.css";
+import Header from "@/components/header";
 
 
 export default function RootLayout({
@@ -8,8 +9,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <head>
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
+      </head>
       <body>
+        <Header />
         {children}
       </body>
     </html>
--- frontend_e-store/src/components/header.tsx
+++ frontend_e-store/src/components/header.tsx
@@ -1,9 +1,34 @@
+import React from "react";
+
+
+
 
 
 export default function Header() {    
     return(
         <header>
+            <nav className="flex justify-between items-center text-black py-6 px-8 md:px-32 bg-white drop-shadow-md">
+                <a href="/">
+                    <img src="/img/logo.svg" alt="logo cyber" className="hover:scale-105 transition-all"/>
+                </a>
+                
+                <div className="relative hidden md:flex items-center justify-center gap-3">
+                    <i className="bx bx-search absolute left-3 text-2xl text-gray-500"></i>
+                    <input type="text" placeholder="Search..." className="py-2 pl-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500" />
+                </div>
+
+                <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
+                    <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md cursor-pointer">Home</li>
+                    <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md cursor-pointer">About</li>
+                    <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md cursor-pointer">Contact Us</li>
+                    <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md cursor-pointer">Blog</li>
+                    <li><img src="/img/like.svg" alt="favorites" /></li>
+                    <li><img src="/img/cart.svg" alt="car" /></li>
+                    <li><img src="/img/profile.svg" alt="profile" /></li>
+                </ul>
             
+                <i className="bx bx-menu xl:hidden block text-5xl cursor-pointer"></i>
+            </nav>
         </header>
     );
 }
