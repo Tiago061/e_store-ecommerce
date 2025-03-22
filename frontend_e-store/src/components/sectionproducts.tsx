6e7b9d00f0df86657d204729476d17b69c36a35c
semnic <thenriqueleite277@gmail.com>
2025-03-22 16:52:20 -0300

    refactor: section products

--- frontend_e-store/src/components/sectionproducts.tsx
+++ frontend_e-store/src/components/sectionproducts.tsx
@@ -19,21 +19,23 @@ export default function SectionProducts(){
                         <p className="font-inter font-medium text-[#909090] text-center">An immersive way to experience entertainment</p>
                     </div>
                 </div>
-                <div className="flex flex-col items-center flex-wrap lg:flex-rowgap-4 p-8 lg:col-start-1 lg:flex lg:col-end-4 ">
+                <div className="flex flex-col items-center flex-wrap gap-4 p-8 lg:col-start-1 lg:flex-row lg:col-end-3 ">
                     <img src="/img/products/PlayStation.webp" alt="PlayStation 5" className="hover:scale-105 lg:hidden "/>
                     <img src="/img/products/PlayStation-desk.webp" alt="" className="hidden lg:block lg:relative lg:right-57"/>
-                    
                         <p className="font-inter-thin font-thin text-black text-[34px]">Playstation <b className="font-bold">5</b></p>
-                        <p className="font-inter font-medium text-[#909090] text-center">Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.</p>
-                   
+                        <p className="font-inter font-medium text-[#909090] text-center lg:float-left">Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.</p>
+                        
                 </div>
                 <div className="flex flex-col items-center flex-wrap bg-[#EDEDED] gap-4 p-8 lg:row-span-2 lg:flex-row-reverse">
                     <img src="/img/products/MacBook-Pro-14.webp" alt="MacBook Pro 14" className="hover:scale-105 lg:hidden"/>
                     <img src="/img/products/macbook-desk.webp" alt="" className="hidden lg:block lg:relative lg:left-8"/>
-                    <p className="font-inter-thin font-thin text-black text-[34px]"><b className="font-bold">Macbook</b> Air</p>
-                    <p className="font-inter font-medium text-[#909090] text-center">The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.</p>
+                    <div className="lg:flex lg:flex-col lg:justify-start lg:flex-wrap lg:items-center lg:relative right ">
+                        <p className="font-inter-thin font-thin text-black text-[34px]"><b className="font-bold">Macbook</b> Air</p>
+                        <p className="font-inter font-medium text-[#909090] text-center ">The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.</p>
+                        <Button borderColor="border-black" size="w-[343px]" text="text-black"/>
+                     </div>
                     
-                    <Button borderColor="border-black" size="w-[343px]" text="text-black"/>
+                   
                 </div>
             </article>
     );
