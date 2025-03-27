1ec2ed43b4c65703f6923914bf6ac7dec22bedf7
semnic <thenriqueleite277@gmail.com>
2025-03-27 20:03:18 -0300

    feat: category for desktop

--- frontend_e-store/src/components/category.tsx
+++ frontend_e-store/src/components/category.tsx
@@ -5,7 +5,7 @@ export default function Category(){
     return(
         <section className="pt-16 pb-16 bg-[#FAFAFA]">
             <div className="flex justify-around">
-                <h2 className="font-inter text-2xl">Browse By Category</h2>
+                <h2 className="font-inter text-2xl lg:pr-[20rem]">Browse By Category</h2>
                 <div className="flex gap-7">
                     <Image 
                         src="/img/categorys/arrowleft.png"
@@ -26,13 +26,13 @@ export default function Category(){
                  </div>
           </div>
 
-            <article className="flex justify-center gap-3.5 pt-12">
-                <div className="flex flex-col gap-4">
+            <article className="flex justify-center gap-3.5 pt-12 lg:flex-row">
+                <div className="flex flex-col gap-4 lg:flex-row">
                     <CategoryItemProps icon="/img/categorys/Phones.png" title="Phones"/>
                     <CategoryItemProps icon="/img/categorys/Cameras.png" title="Cameras"/>
                     <CategoryItemProps icon="/img/categorys/Computers.png" title="Computers"/>
                 </div>
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4 lg:flex-row">
                     <CategoryItemProps icon="/img/categorys/Smart Watches.png" title="Smart Watches"/>
                     <CategoryItemProps icon="/img/categorys/Headphones.png" title="Headphones"/>
                     <CategoryItemProps icon="/img/categorys/Gaming.png" title="Gaming"/>
